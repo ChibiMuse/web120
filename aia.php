2d697f7e626cb2ee785edc1c12ed592dcd47fb3c
ChibiMuse <megan.smith@live.com>
2020-02-17 17:49:46 -0800

    Adds Tables to AIA
    Adds table to the AIA page w/ minimum content. Need to style

--- aia.php
+++ aia.php
@@ -1,10 +1,55 @@
 <?php include "includes/header.php";?>
-    <main  class="wrapper">
-          <section class="aia">
-              <h2>Lorem Ipsum</h2>
-              <h3>"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."</h3>
-              <h4>"There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain..."</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed interdum est, eget pharetra tortor. Nunc ante ligula, vehicula vitae ultricies volutpat, pretium cursus ipsum. Proin vel porta nisl, non accumsan erat. Nunc lectus mi, aliquam a rhoncus quis, luctus nec velit. In sed vehicula sem. Morbi ultricies sem at justo mollis iaculis. Vivamus nec varius orci. Cras bibendum fermentum tellus et finibus. Morbi egestas felis vel scelerisque dictum. Duis eu porta tellus.</p>
+    <main  class="wrapper container">
+          <section class="client-information">
+              <h2>Audience, Issues &amp; Approach</h2>
+              
+                <table id="client-info" class= "client">
+                    <tr>
+                        <th class = "label1 client">Client</th>
+                        <td class = "content1 client">Trevor H.</td>
+                    </tr>
+                    <tr>
+                        <th class = "label2 website-name">Website</th>
+                        <td class = "content2 website-name">Press Start</td>
+                    </tr>
+                    <tr>
+                        <th class = "label3 website-status">Redesign/New</th>
+                        <td class = "content3 website-status">New</td>
+                    </tr>
+                    <tr>
+                        <th class = "label4 website-type">Website Type</th>
+                        <td class = "content4 website-type">Information/Entertainment</td>
+                    </tr>
+                    <tr>
+                        <th class = "label5 goals">Goals</th>
+                        <td class = "content5 goals">Fullfil hobby interests and have a website about the history of one of their favorite gaming companites to share with others.</td>
+                    </tr>
+                </table>
+            </section>
+            <section class = "client-audiences">
+                <h2 class = "user-stories">User Stories</h2>
+                <table id="client-audience" class ="user-stories">
+                    <tr>
+                        <th class = "header2 issues">Issues, Concerns, Needs</th>
+                        <th class = "header3 approach">Approach</th>
+                        <th class = "header1 audiences">Audiences</th>
+                    </tr>
+                    <tr>
+                        <td class = "audience1 audiences">TestA</td>
+                        <td class = "issue1 issues">Test1</td>
+                        <td class = "approach1 approach">TestX</td>                    
+                    </tr>
+                    <tr>
+                        <p class = "audience2 audiences">TestB</p>
+                        <p class = "issue2 issues">Test2</p>
+                        <p class = "approach2 approach">TestY</p>
+                    </tr>
+                    <tr>
+                        <p class = "audience3 audiences">TestC</p>
+                        <p class = "issue3 issues">Test3</p>
+                        <p class = "approach3 approach">TestZ</p>
+                    </tr>
+                </table>
            </section>
     </main>
 <?php include "includes/footer.php";?>
